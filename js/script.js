c363c87d97bcd0b77971588283d35451d1489c06
dennis <dennis.olimpio0794@gmail.com>
2020-07-07 16:45:50 +0200

    add funzione per  salvare i valori della input sul server. Aggiungo tasto elimina

---
+++ js/script.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+
+// richiamo l'api
+
+  todoList ();
+
+  $("#addText").click( function (){
+
+
+
+  var input = $("input").val();
+
+  if ( input.length > 0 ) {
+
+    $.ajax ( {
+
+      url: "http://157.230.17.132:3021/todos",
+      method: "POST", // leggo
+      data: {
+        text: input
+      },
+      success: function(dataResponse) {
+
+        console.log(dataResponse)
+
+      },
+
+      error: function () {
+
+
+      }
+
+
+
+    })
+
+  } else {
+
+    alert( " attenzione, il campo di testo è vuoto");
+  }
+
+
+
+
+
+  // reset();
+
+});
+
+
+});
+
+// fine document ready
+
+
+
+
+// ------- funzioni -------
+
+// con questa funzione richiamo la mia api
+
+function todoList () {
+
+
+  $.ajax( {
+
+    url: "http://157.230.17.132:3021/todos",
+    method: "GET", // leggo
+    success: function(dataResponse) {
+
+      if( dataResponse.length > 0) {
+
+        var source = $("#list-template").html();
+        var template = Handlebars.compile(source);
+
+        for (var i = 0; i < dataResponse.length; i++) {
+
+          var cosaDaFare = dataResponse[i];
+          var html = template(cosaDaFare);
+
+
+          $("#todo-list").append(html);
+        }
+
+      }
+
+
+    },
+    error: function () {
+
+      console.log("si è verificato un errore")
+
+    }
+
+  })
+
+}
+
+//reset
+function reset () {
+
+  $("#todo-list").html("");
+
+}
+
+// salvo i valori della input nella Api cosi da vederlo anche al refresh della pagina
