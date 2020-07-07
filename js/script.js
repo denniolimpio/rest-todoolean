18d38c37c9b1e57077b6f705a5d5a97395fddb99
dennis <dennis.olimpio0794@gmail.com>
2020-07-07 17:28:48 +0200

    ho aggiunto l'evento al click del tasto delete ( X )

--- js/script.js
+++ js/script.js
@@ -1,51 +1,29 @@
 $(document).ready(function () {
 
-// richiamo l'api
-todoList ();
-
-
-  $("#addText").click( function (){
-
-
-
-  var input = $("input").val();
-
-  if ( input.length > 0 ) {
-
-    $.ajax ( {
-
-      url: "http://157.230.17.132:3021/todos",
-      method: "POST", // leggo
-      data: {
-        text: input
-      },
-
-      success: function(dataResponse) {
-
-
-      },
-
-      error: function () {
-
-
-      }
-
-
-
-    })
-
-  } else {
-
-    alert( " attenzione, il campo di testo è vuoto");
-  }
-
-
-
+  // dichiaro le mie variabili:
+  var aggiungi = $('#addText');
+  var elimina = $('#deleteText');
+  // richiamo l'api con l'elenco delle cose da cosaDaFare
   todoList ();
 
 
+// Quando clicco sul bottone aggiungi  genero un evento:
+// -- > salvo il contenuto della input sul server     saveInput ()
+// -- > refresho la pagina e mostro il nuovo elemento a schermo
+    aggiungi.click( function (){
+    saveInput ()
+    todoList ();
+  });
 
-});
+
+// Quando clicco sul bottone delete ( X )  genero un evento:
+// -- > elimino l'elemento cliccato
+  $(document).on('click', elimina, function () {
+
+    alert ( "hai cliccato elimina");
+    
+  });
+
 
 
 });
@@ -57,12 +35,12 @@ todoList ();
 
 // ------- funzioni -------
 
-// con questa funzione richiamo la mia api
+
+// con questa funzione richiamo la mia api porta :3021/todos
 
 function todoList () {
 
   reset();
-
 
   $.ajax( {
 
@@ -83,9 +61,7 @@ function todoList () {
 
           $("#todo-list").append(html);
         }
-
       }
-
 
     },
     error: function () {
@@ -98,7 +74,40 @@ function todoList () {
 
 }
 
-//reset
+
+// Creo una funzione che mi permette di salvare il contenuto della input sul server
+
+function saveInput () {
+
+ // leggo il contenuto della input
+  var input = $("input").val();
+
+  if ( input.length > 0 ) {
+
+    $.ajax ( {
+
+      url: "http://157.230.17.132:3021/todos",
+      method: "POST", // leggo
+      data: {
+        text: input
+      },
+      success: function(dataResponse) {
+      },
+
+      error: function () {
+
+      }
+
+    })
+
+  } else {
+
+    alert( " attenzione, il campo di testo è vuoto");
+  }
+}
+
+
+//reset, refresho in contenuto della pagina ogni volta che aggiungo un nuovo elemento nella lista
 function reset () {
   $("#todo-list").html("");
 
